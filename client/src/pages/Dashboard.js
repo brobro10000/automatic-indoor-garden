6a36be84bba7cd4368c4a3eca921bddc1536f377
Hamzah Ullah <hamzahullah@yahoo.com>
2022-08-26 00:17:19 -0400

    added redirects

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,15 @@
 
+import Auth from "../utils/auth";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 const Dashboard = () => {
-
+    const [loggedIn,] = useState(Auth.loggedIn())
+    const navigate = useNavigate()
+    useEffect(() =>{
+        if(!loggedIn){
+            navigate('/')
+        }
+    });
     return (
         <div className="background">
             <h1>Dashboard</h1>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,21 @@
 // import StarterModal from "./Modal";
+import { useState, useEffect } from 'react'
 import LoginModal from "../components/loginModal/index.js";
+import Auth from "../utils/auth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-
+    const [loggedIn,] = useState(Auth.loggedIn())
+    const navigate = useNavigate()
+    useEffect(() =>{
+        if(loggedIn){
+            navigate('/dashboard')
+        }
+    });
     return (
-        <div className="background">
-            <LoginModal />
-        </div>
+            <div className="background">
+                <LoginModal />
+            </div>
     );
 };
 
